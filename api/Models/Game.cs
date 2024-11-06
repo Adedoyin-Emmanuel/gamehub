172a6b262f271016c3df5ccf8ec0b31d5fcf4ab6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-06 16:02:45 +0100

    feat: finished game model

---
+++ api/Models/Game.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models;
+
+
+public class Game
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Genre { get; set; }
+    public required string ImageUrl { get; set; }
+    
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; }
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime UpdatedAt { get; set; }
+}
