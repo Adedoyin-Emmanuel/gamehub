ff69f8d020f9f5a5fdea2518074d393b08b0bdb9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-06 06:12:26 +0100

    feat: added pomelo EfCore Mysql package

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -16,10 +16,12 @@ public class WeatherForecastController : ControllerBase
     {
         _logger = logger;
     }
+    
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
