34738be9430b8e422c44bd0cc6cc57300acf490d
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-07 16:27:48 +0100

    feat: added Response extension

---
+++ api/Extensions/Response.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using Newtonsoft.Json;
+
+namespace api.Extensions;
+
+public class Response
+{
+    public int Code { get; private set; }
+
+    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+    public string Message { get; private set; }
+
+    public object? Data { get; private set; }
+
+
+    public Response(int code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+
+    public Response(string message) : this(200, message)
+    {
+        Message = message;
+    }
+
+
+    public Response(string message, object data) : this(200, message)
+    {
+        Data = data;
+    }
+
+
+}
