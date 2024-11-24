7bb080342396bfed795ad0dbc655caa00b1e5737
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 18:08:34 +0100

    feat: updated game component

--- frontend/src/components/game/index.tsx
+++ frontend/src/components/game/index.tsx
@@ -1,26 +1,30 @@
 import { Badge } from "../ui/badge";
+import { Link } from "react-router-dom";
 interface GameProps {
   name: string;
   imageUrl: string;
   genre: string;
+  gameId: string;
 }
 
-const Game = ({ name, imageUrl, genre }: GameProps) => {
+const Game = ({ name, imageUrl, genre, gameId }: GameProps) => {
   return (
-    <div className="border rounded-md md:w-48 cursor-pointer">
-      <img
-        src={imageUrl}
-        alt={`Image relating to ${name}`}
-        className="rounded-t-md"
-      />
+    <Link to={`/${gameId}`}>
+      <div className="border rounded-md md:w-48 cursor-pointer">
+        <img
+          src={imageUrl}
+          alt={`Image relating to ${name}`}
+          className="rounded-t-md"
+        />
 
-      <div className="flex items-center justify-between p-1">
-        <p className="font-semibold py-2 truncate">{name}</p>
-        <Badge className="capitalize" variant={"outline"}>
-          {genre}
-        </Badge>
+        <div className="flex items-center justify-between p-1">
+          <p className="font-semibold py-2 truncate">{name}</p>
+          <Badge className="capitalize" variant={"outline"}>
+            {genre}
+          </Badge>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
