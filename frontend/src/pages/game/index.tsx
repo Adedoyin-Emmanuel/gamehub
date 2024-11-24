f2e94cf01341005abbbfa020433451f0690cad0a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 17:00:35 +0100

    feat: added dayjs

--- frontend/src/pages/game/index.tsx
+++ frontend/src/pages/game/index.tsx
@@ -1,9 +1,21 @@
-
+import Navbar from "@/components/navbar";
 
 const GamePage = () => {
   return (
-    <div>
-      <h1>Game works!</h1>
+    <div className="w-full">
+      <Navbar />
+
+      <div className="w-[900px] flex items-center justify-center flex-col mx-auto my-3">
+        <img
+          src="https://github.com/adedoyin-emmanuel.png"
+          alt="My Github image"
+          className="rounded-full"
+          width={300}
+          height={300}
+              />
+              
+              
+      </div>
     </div>
   );
 };
