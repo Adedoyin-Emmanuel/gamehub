cb4be0f0712cb6f87ec9fba66b845f4f535b10e0
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 17:58:34 +0100

    feat: added game image upload component

---
+++ frontend/src/components/game-image-upload/index.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface GameImageUploadProps {
+    
+}
+
+const GameImageUpload = () => {
+
+    return (
+        <div>
+            <h1>GameImageUpload works!</h1>
+        </div>
+    );  
+}
+
+export default GameImageUpload;
+    
