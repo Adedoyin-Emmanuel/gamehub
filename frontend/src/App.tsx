7b0e7cfc05adf92f6d4be06db2aec50fb040641b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 18:36:16 +0100

    feat: added tanstack dev tools

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,21 @@ import "./App.css";
 import GamePage from "./pages/game";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<GamePage/>} path="/:gameId" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<GamePage />} path="/:gameId" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
