64043d3b9878ede1cdd18bc419f658f498c7bf1c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 17:20:44 +0100

    feat: updated game navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
-
+import { Button } from "../ui/button";
 const Navbar = () => {
   return (
-    <div className={"border p-3"}>
+    <div className={"w-full border p-3 flex items-center justify-between"}>
       <Link to={"/"}>
         <h2 className={"text-2xl font-bold"}>Gamehub</h2>
       </Link>
+
+      <Button>Add new game</Button>
     </div>
   );
 };
