02bf536e957c05a54624e05b078baaccffcc26a1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 19:01:35 +0100

    feat: added loader

--- frontend/src/components/loader/index.tsx
+++ frontend/src/components/loader/index.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { ClipLoader } from "react-spinners";
+import { cn } from "@/lib/utils";
 
 interface LoaderProps {
   isLoading: boolean;
+  size?: number;
 }
 
-const Loader = ({isLoading}: LoaderProps) => {
+export const Loader = ({ isLoading, size = 27 }: LoaderProps) => {
   return (
-    <div>
-      <h1>Loader works!</h1>
-    </div>
+    <>
+      <div
+        className={cn(
+          "fixed inset-0 z-[1000] bg-black/40  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+        )}
+      ></div>
+      <div className="w-screen h-screen flex items-center justify-center fixed ">
+        <ClipLoader loading={isLoading} color="#5A83ED" size={size} />
+      </div>
+    </>
   );
 };
-
 export default Loader;
