166a38dd80bfe935d99e5ec6880a081e33d8e4f2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 14:36:56 +0100

    feat: added game component

---
+++ frontend/src/components/game/index.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface GameProps {
+    
+}
+
+const Game = () => {
+
+    return (
+        <div>
+            <h1>Game works!</h1>
+        </div>
+    );  
+}
+
+export default Game;
+    
