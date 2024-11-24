db4a4d67dbdcb27ee1967f427c65182e14580df8
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 17:24:44 +0100

    feat: added game modal component

---
+++ frontend/src/components/gamemodal/index.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface GamemodalProps {
+    
+}
+
+const Gamemodal = () => {
+
+    return (
+        <div>
+            <h1>Gamemodal works!</h1>
+        </div>
+    );  
+}
+
+export default Gamemodal;
+    
