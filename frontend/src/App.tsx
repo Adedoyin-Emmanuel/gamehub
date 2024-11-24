b0ab0d55b3b62020fc6683a82cb19e9eddb8062f
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-24 14:45:05 +0100

    feat: added badge component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "@/components/navbar";
+import Game from "@/components/game";
 
 function App() {
   return (
     <div className="w-full">
       <Navbar />
+      <br />
+      <Game
+        name="Codm"
+        imageUrl="https://github.com/adedoyin-emmanuel.png"
+        genre="action"
+      />
     </div>
   );
 }
--- frontend/src/components/game/index.tsx
+++ frontend/src/components/game/index.tsx
@@ -1,18 +1,24 @@
-
-import React from "react";
-
 interface GameProps {
-    
+  name: string;
+  imageUrl: string;
+  genre: string;
 }
 
-const Game = () => {
+const Game = ({ name, imageUrl, genre }: GameProps) => {
+  return (
+    <div className="border rounded-md w-48">
+      <img
+        src={imageUrl}
+        alt={`Image relating to ${name}`}
+        className="rounded-t-md"
+      />
 
-    return (
-        <div>
-            <h1>Game works!</h1>
-        </div>
-    );  
-}
+      <div>
+        <p className="font-semibold py-2">{name}</p>
+        <p className="capitalize py-3">{genre}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Game;
-    
