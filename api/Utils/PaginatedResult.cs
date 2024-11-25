00f4595e238787567a4907f8c164afb76d33dd2c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-25 15:33:44 +0100

    feat: updated paginated result class

--- api/Utils/PaginatedResult.cs
+++ api/Utils/PaginatedResult.cs
@@ -1,6 +1,9 @@
 namespace api.Utils;
 
-public class PaginatedResult
+public class PaginatedResult<T>
 {
-    
+    public List<T> Data { set; get; } = [];
+    public int Total { set; get; }
+    public int Skip { get; set; }
+    public int Take { get; set; }
 }
