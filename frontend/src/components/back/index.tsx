957c9029159aaba5c167e4f51adf4c55eba3933e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-11-25 16:47:27 +0100

    feat: added back component

---
+++ frontend/src/components/back/index.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface BackProps {
+    
+}
+
+const Back = () => {
+
+    return (
+        <div>
+            <h1>Back works!</h1>
+        </div>
+    );  
+}
+
+export default Back;
+    
